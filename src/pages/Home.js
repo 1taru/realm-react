600aa4cd455168676c6d17dda4b0a874432c35a2
Nicolas Fernandez Clai <taru@MacBook-Air-de-Nicolas.local>
2024-08-20 23:39:02 -0400

    Añadí y modifiqué archivos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,15 @@ import '.././App.css'; // Asegúrate de importar el archivo CSS
 
 function Home() {
   return (
+    <div>
+      <p>a</p>
+    
     <div className="page-container">
     <div className="home">
       <h1>Bienvenido a Realm</h1>
       <p>Explora nuestra colección de ropa de moda.</p>
       {/* Aquí puedes agregar secciones destacadas, sliders, etc. */}
+    </div>
     </div>
     </div>
   );
